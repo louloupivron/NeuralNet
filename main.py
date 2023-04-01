0377cdad64d9d7f1c1a0a74fc3ede33e52edc76b
louloupivron <louis.pivron@epfl.ch>
2023-04-01 23:08:09 +0200

    creating first patterns

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+#####
+#imports
+#####
+
+import numpy as np
+import cmath
+import HopfieldNetwork as hn
+import HebbianLearningRule as hl
+
 
 
 if __name__ == '__main__':
 	print('main.py is being run directly')
+
+	HopfieldN = hn.HopfieldNetwork(50,3)
+	HopfieldN.printPatterns()
+	HopfieldN.learn(type='hebbian')
+
+
+
+
+
 
 else:
 	print('main.py has been imported')
